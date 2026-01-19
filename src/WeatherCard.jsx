d29a2cf1200d4a4002dde66203a99923db64b574
leen fani <leenfani06@gmail.com>
2026-01-19 23:23:50 +0200

    feat: add button to change the language

--- src/WeatherCard.jsx
+++ src/WeatherCard.jsx
@@ -2,55 +2,68 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { Typography } from "@mui/material";
 import { Grid } from "@mui/material";
-import CloudIcon from '@mui/icons-material/Cloud';
+import CloudIcon from "@mui/icons-material/Cloud";
+import { Button } from "@mui/material";
 
 export default function WeatherCard() {
   return (
-    <div className="grid place-items-center min-h-screen">
-      <Container
-        fixed
-        maxWidth="sm"
-        style={{
-          backgroundColor: "#049bff",
-          borderRadius: "25px",
-          boxShadow: "8px 11px 2px rgba(0,0,0,0.09)",
-        }}
-      >
-        <Grid container spacing={2} sx={{ flexGrow: 1, direction: "rtl" }}>
-          {/* cityName + date */}
-          <Grid
-            size={12}
-            className="flex items-baseline gap-5 border-b-4 pr-5 pt-5"
-          >
-            <Typography variant="h1"> لندن</Typography>
-            <Typography variant="h5"> يناير 2026</Typography>
-          </Grid>
-
-          {/* detailes */}
-          <Grid container size={6} className="flex items-baseline gap-5">
-            <div>
-              <div>
-                <Typography variant="h1">38</Typography>
-                {/* tempreture state image */}
-              </div>
-              <Typography variant="h6" className="pb-5">
-                broken cloues
+    <Box
+      minHeight="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box display="flex" flexDirection="column" alignItems="flex-start">
+        <Container
+          fixed
+          maxWidth="sm"
+          sx={{
+            backgroundColor: "#049bff",
+            borderRadius: "25px",
+            boxShadow: "8px 11px 2px rgba(0,0,0,0.09)",
+          }}
+        >
+          <Grid container spacing={2} sx={{ direction: "rtl" }}>
+            {/* cityName + date */}
+            <Grid
+              size={12}
+              sx={{
+                display: "flex",
+                alignItems: "flex-end",
+                gap: 2,
+                borderBottom: 4,
+                pr: 2,
+                pt: 2,
+              }}
+            >
+              <Typography variant="h1">لندن</Typography>
+              <Typography sx={{ mb: "7px" }} variant="h5">
+                يناير 2026
               </Typography>
-              <div className="flex items-baseline gap-2">
-                <Typography variant="h6" className="pb-5">
-                  18 صغرى |
-                </Typography>
-                <Typography variant="h6" className="pb-5">22 كبرى</Typography>
-              </div>
-            </div>
-          </Grid>
+            </Grid>
 
-          {/* icon */}
-          <Grid size={6}>
-            <CloudIcon style={{fontSize:"200px"}}/>
+            {/* details */}
+            <Grid size={6}>
+              <Typography variant="h1">38</Typography>
+              <Typography variant="h6">broken clouds</Typography>
+
+              <Box display="flex" gap={1}>
+                <Typography variant="h6">18 صغرى |</Typography>
+                <Typography variant="h6">22 كبرى</Typography>
+              </Box>
+            </Grid>
+
+            {/* icon */}
+            <Grid size={6} display="flex" justifyContent="center">
+              <CloudIcon sx={{ fontSize: 200 }} />
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-    </div>
+        </Container>
+
+        <Button variant="text" sx={{ mt: 1 }}>
+          language
+        </Button>
+      </Box>
+    </Box>
   );
 }
